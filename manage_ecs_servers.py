4ef2251484e577403aba648067e5391612dc805e
Alexia Jauregui <alexia.jauregui@integratel.com.pe>
2026-05-14 16:19:43 -0500

    ya no doy mas x2222222

--- manage_ecs_servers.py
+++ manage_ecs_servers.py
@@ -149,7 +149,7 @@ class ECSScheduler:
     def _list_servers_rest(self, name_filter: str) -> list:
         token  = self._get_iam_token()
         url    = f"https://ecs.{self.region}.myhuaweicloud.com"
-        url   += f"/v2.1/{self.project_id}/servers/detail"
+        url   += f"/v2/{self.project_id}/servers/detail"
         params = {"name": name_filter} if name_filter else {}
 
         resp = requests.get(url, headers={"X-Auth-Token": token}, params=params, timeout=30)
@@ -205,7 +205,7 @@ class ECSScheduler:
     def _start_rest(self, targets: list):
         token   = self._get_iam_token()
         url     = f"https://ecs.{self.region}.myhuaweicloud.com"
-        url    += f"/v2.1/{self.project_id}/servers/action"
+        url    += f"/v2/{self.project_id}/servers/action"
         payload = {
             "os-start": {
                 "servers": [{"id": s["id"]} for s in targets]
@@ -256,7 +256,7 @@ class ECSScheduler:
     def _stop_rest(self, targets: list):
         token   = self._get_iam_token()
         url     = f"https://ecs.{self.region}.myhuaweicloud.com"
-        url    += f"/v2.1/{self.project_id}/servers/action"
+        url    += f"/v2/{self.project_id}/servers/action"
         payload = {
             "os-stop": {
                 "type"   : "SOFT",
